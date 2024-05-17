011a3dba8cbdd24e712614a0a829d80082e11899
Suba Dániel <danisuba10@gmail.com>
2024-05-17 11:18:59 +0300

    find all hotels

--- src/main/java/com/hotelmanager/service/HotelService.java
+++ src/main/java/com/hotelmanager/service/HotelService.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.hotelmanager.repo.HotelRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,5 +22,16 @@ public class HotelService {
 
     public Optional<Hotel> findById(int id){
         return this.hotelRepository.findById(id);
+    }
+
+    public List<Hotel> findAll()
+    {
+        List<Hotel> result = new ArrayList<Hotel>();
+        Iterable<Hotel> iterHotels =  this.hotelRepository.findAll();
+        for(Hotel hotel : iterHotels)
+        {
+            result.add(hotel);
+        }
+        return result;
     }
 }
